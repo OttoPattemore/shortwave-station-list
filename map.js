ef217e236ee43172611dd45f1d8dedabb47b8f87
Otto Pattemore <otto.pattemore@gmail.com>
2022-05-02 08:18:51 +1000

    Update map

--- map.js
+++ map.js
@@ -1,10 +1,7 @@
 var map = L.map('map').setView([0, 0], 2);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
     id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
 }).addTo(map);
 
 async function getStationList(){
